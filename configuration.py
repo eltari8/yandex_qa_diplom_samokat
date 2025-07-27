6cc1446e50c15da0f2991d31a4980917d038d081
Natalia Lebedeva <Eltari8@ya.ru>
2025-07-27 22:18:06 +0300

    Ready to submit my project

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-MAIN_URL = "https://b6d0ec6e-099b-4f32-8d75-d011c5ba20db.serverhub.praktikum-services.ru"  #не забывай менять при перезапуске сервера !!без слеша в конце!
+MAIN_URL = "https://8f9e2a4b-4e38-4446-8950-732174c5d2cb.serverhub.praktikum-services.ru"  #не забывай менять при перезапуске сервера !!без слеша в конце!
 CREATE_ORDER_URL = "/api/v1/orders"  # ручка создания заказа
 RECEVE_Order_Track_URL = "/api/v1/orders/track"  # ручка получения заказа по его треку
